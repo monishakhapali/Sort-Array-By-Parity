4c4dd72e900033a88c389906e9a7f2e664ce5ae4
Monisha Khapali <monisha.khapali@logicsolutions.com>
2020-12-15 15:25:12 -0500

    added files

---
+++ SortArrayByParity/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortArrayByParity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] A = { 3, 1, 2, 4 };
+            SortArrayParity(A);
+
+        }
+    
+        public static int[] SortArrayParity(int[] A)
+        {
+            List<int> B = new List<int>();
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] % 2 == 0)
+                    B.Add(A[i]);
+
+            }
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] % 2 != 0)
+                    B.Add(A[i]);
+
+            }
+
+            return B.ToArray();
+
+        }
+
+    }
+}
